c46d67c83523e4472002459dfc83a8346b4bab07
Kirill Karpov <karpov.kir.v@gmail.com>
2024-04-12 16:56:25 +0400

    Fix Prettier config

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,11 +1,11 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   verbose: true,
-  preset: "ts-jest/presets/default-esm",
-  testEnvironment: "jsdom",
-  testMatch: ["<rootDir>/src/**/*.(test).*"],
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'jsdom',
+  testMatch: ['<rootDir>/src/**/*.(test).*'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: {
-    "(.+)\\.js": "$1",
+    '(.+)\\.js': '$1',
   },
 };
--- src/testUtils/queries.ts
+++ src/testUtils/queries.ts
@@ -2,8 +2,8 @@
  * https://testing-library.com/docs/react-testing-library/api/#queries
  */
 
-import { queryHelpers } from "@testing-library/dom";
-import { queries as testingLibraryQueries } from "@testing-library/react";
+import { queryHelpers } from '@testing-library/dom';
+import { queries as testingLibraryQueries } from '@testing-library/react';
 
 function getAllBySelector(container: HTMLElement, selector: string) {
   return Array.from(container.querySelectorAll<HTMLElement>(selector));
@@ -13,10 +13,7 @@ function getBySelector(container: HTMLElement, selector: string) {
   const result = getAllBySelector(container, selector);
 
   if (result.length > 1) {
-    throw queryHelpers.getElementError(
-      `Found multiple elements with the selector ${selector}`,
-      container,
-    );
+    throw queryHelpers.getElementError(`Found multiple elements with the selector ${selector}`, container);
   }
   return result[0] || null;
 }
