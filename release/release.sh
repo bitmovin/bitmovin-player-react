1e9c6efd4ce335d70a31688c69f0c1df13bc106e
Kirill Karpov <karpov.kir.v@gmail.com>
2024-04-15 14:01:42 +0400

    Simplify release.sh

--- release/release.sh
+++ release/release.sh
@@ -1,29 +1,16 @@
 #!/bin/bash
 
-nonInteractive=false
 dryRun=true
 
 if [ -z "$NPM_PUBLISH_TOKEN" ]; then
-    read -s -p "Enter your NPM access token: " NPM_PUBLISH_TOKEN
-    echo
+  echo "NPM_PUBLISH_TOKEN is missing"
+  exit 1
 fi
 
 # Check if we are on the main branch.
 if [ "$(git rev-parse --abbrev-ref HEAD)" != "main" ]; then
     echo "You must be on the main branch to publish to NPM"
     exit 1
-fi
-
-version=`git describe --tags --abbrev=0`
-echo "Retrieved version from 'git describe'. Make sure this is correct :"
-echo $version
-if [ "$nonInteractive" = false ] ; then
-  read -p "(Press enter to continue)"
-fi
-
-echo "Publishing to NPM..."
-if [ "$nonInteractive" = false ] ; then
-  read -p "(Press enter to continue)"
 fi
 
 npm config set registry https://registry.npmjs.org
