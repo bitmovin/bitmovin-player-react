1fa522fd6c63e88edf8798490f20000f9e1286e2
Kirill Karpov <karpov.kir.v@gmail.com>
2024-04-15 16:19:02 +0400

    Update docs

--- release/README.md
+++ release/README.md
@@ -7,4 +7,4 @@
 - Commit with a message with the content `Release <X.Y.Z>` and push to the `main` branch
 - Create a new tag with the release version (`git tag <X.Y.Z> main`)
 - Push the new tag to the repository (`git push origin <X.Y.Z>`)
-- Publish the package to NPM using the Release GH action
+- Publish the package to the NPM registry using the Release GH action
--- release/release.sh
+++ release/release.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# Publish the package to NPM registry.
+# Publishes the package to the NPM registry.
 # Uses the latest GIT tag as the package version, the GIT tag should be in the format `1.0.0` or `1.0.0-beta.0`.
+#
 # Expected environment variables:
 # - NPM_PUBLISH_TOKEN: [REQUIRED] The NPM access token to publish the package.
 # - DRY_RUN: [OPTIONAL] If set to `true`, the package is not published to the NPM registry (the command is executed as a dry run).
@@ -19,7 +20,7 @@ fi
 
 # Check if we are on the main branch.
 if [ "$(git rev-parse --abbrev-ref HEAD)" != "main" ]; then
-    echo "You must be on the main branch to publish to NPM"
+    echo "You must be on the main branch to publish to the NPM registry."
     exit 1
 fi
 
