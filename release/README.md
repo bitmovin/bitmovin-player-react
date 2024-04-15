f24cfa0ed411c0f9a4894baef4a59124f1f703e5
Kirill Karpov <karpov.kir.v@gmail.com>
2024-04-15 16:04:22 +0400

    Automatically update version in package.json

--- release/README.md
+++ release/README.md
@@ -4,7 +4,6 @@
 - Update the `CHANGELOG.md`
   - Create a new section with the release version in the `Released` section
   - Move the content of the `Unreleased` section to the new section
-- Update the version property in the `package.json` file
 - Commit with a message with the content `Release <X.Y.Z>` and push to the `main` branch
 - Create a new tag with the release version (`git tag <X.Y.Z> main`)
 - Push the new tag to the repository (`git push origin <X.Y.Z>`)
--- release/release.sh
+++ release/release.sh
@@ -7,11 +7,22 @@ if [ -z "$NPM_PUBLISH_TOKEN" ]; then
   exit 1
 fi
 
+if ! [ -f ./package.json ]; then
+  echo "Cannot find package.json file. Execute the command from the root of the project."
+  exit 1
+fi
+
+
 # Check if we are on the main branch.
 if [ "$(git rev-parse --abbrev-ref HEAD)" != "main" ]; then
     echo "You must be on the main branch to publish to NPM"
     exit 1
 fi
+
+version=`git describe --tags --abbrev=0`
+
+cat package.json | jq  --arg version "$version" '.version |= $version' > package.temp.json
+mv -f package.temp.json package.json
 
 npm config set registry https://registry.npmjs.org
 
