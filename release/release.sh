2ea0563ef0be8d9fd5a53501cca057aa1caa4b14
Kirill Karpov <85180621+karpov-kir@users.noreply.github.com>
2024-04-16 16:04:25 +0400

    ci: release workflow (#3)

--- release/release.sh
+++ release/release.sh
@@ -1,35 +1,71 @@
 #!/bin/bash
 
-if [ -z "$NPM_TOKEN" ]; then
-    read -s -p "Enter your npm access token: " NPM_TOKEN
-    echo
+# Publishes the package to the NPM registry, commits the changes, and creates a new GIT tag, pushes them to the remote repository.
+#
+# Expected environment variables:
+#   - NPM_PUBLISH_TOKEN: [REQUIRED] The NPM access token to publish the package.
+#   - VERSION: [REQUIRED] The version of the package to publish.
+#   - DRY_RUN: [OPTIONAL] If set to `true`, the package is not published to the NPM registry,
+#     the commit and GIT tag are not pushed to the remote repository (the command is executed as a dry run).
+
+echo "Releasing the package to the NPM registry"
+
+if [ -z "$NPM_PUBLISH_TOKEN" ]; then
+  echo "NPM_PUBLISH_TOKEN is missing"
+  exit 1
 fi
+
+if [ -z "$VERSION" ]; then
+  echo "VERSION is missing"
+  exit 1
+fi
+
+if ! [ -f ./package.json ]; then
+  echo "Cannot find package.json file. Execute the command from the root of the package."
+  exit 1
+fi
+
+if [ "$(git status -s)" ]; then
+  echo "Uncommited changes detected. Please commit your changes before publishing the package."
+  exit 1
+fi
+
+if [ "$DRY_RUN" = true ] ; then
+  echo "!!!! DRY RUN MODE !!!!"
+fi
+
+echo "Updating the CHANGELOG.md file"
+
+currentDate=$(date '+%Y-%m-%d')
+newReleasedContent="## [Unreleased]\n\n## ${VERSION} - ${currentDate}"
+# Replace the current unreleased header with the new released header followed by the new version content.
+sed "s/## \[Unreleased\]/${newReleasedContent}/" ./CHANGELOG.md > CHANGELOG.tmp.md
+mv -f CHANGELOG.tmp.md CHANGELOG.md
+
+git add ./CHANGELOG.md
 
 # Check if we are on the main branch.
 if [ "$(git rev-parse --abbrev-ref HEAD)" != "main" ]; then
-    echo "You must be on the main branch to publish to npm"
+    echo "You must be on the main branch to publish to the NPM registry."
     exit 1
 fi
 
-VERSION=`git describe --tags --abbrev=0`
-echo "Retrieved VERSION from 'git describe'. Make sure this is correct :"
-echo $VERSION
-read -p "(Press enter to continue)"
+echo "Updating the version and committing the changes"
 
-echo "Publishing to NPM..."
-read -p "(Press enter to continue)"
-
-# Set the registry to the public npm registry.
+# Updates the package.json, pacakge-lock.json, files, commits the changes, and creates a new GIT tag.
+# Use `--force` to allow CHANGELOG.md changes to be committed as well.
+npm version "$VERSION" --force
 npm config set registry https://registry.npmjs.org
 
-# Log in to the npm registry using the access token.
-echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" >> ~/.npmrc
+# Log in to the NPM registry using the access token.
+echo "//registry.npmjs.org/:_authToken=${NPM_PUBLISH_TOKEN}" >> ~/.npmrc
 npm whoami || npm login
 
-# Test the publishing process.
-(cd .. && npm publish --dry-run=true)
+echo "Publishing the package to the NPM registry"
 
-read -p "Does the above look correct? (Press enter to continue)"
+npm publish --dry-run="$DRY_RUN"
 
-# Publish the package to the npm registry.
-(cd .. && npm publish)
+if [ "$DRY_RUN" != true ] ; then
+  echo "Pushing the changes to the remote repository"
+  git push --follow-tags
+fi
