3681b70515a29204a0da9e2ea4618cba4779eafd
Kirill Karpov <karpov.kir.v@gmail.com>
2024-04-16 15:56:58 +0400

    Fix sed

--- release/release.sh
+++ release/release.sh
@@ -39,7 +39,8 @@ echo "Updating the CHANGELOG.md file"
 currentDate=$(date '+%Y-%m-%d')
 newReleasedContent="## [Unreleased]\n\n## ${VERSION} - ${currentDate}"
 # Replace the current unreleased header with the new released header followed by the new version content.
-sed -i '' "s/## \[Unreleased\]/${newReleasedContent}/" ./CHANGELOG.md
+sed "s/## \[Unreleased\]/${newReleasedContent}/" ./CHANGELOG.md > CHANGELOG.tmp.md
+mv -f CHANGELOG.tmp.md CHANGELOG.md
 
 git add ./CHANGELOG.md
 
