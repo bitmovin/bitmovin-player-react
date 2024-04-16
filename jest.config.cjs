df4c7e96c467fc2993a3b32713f11ee32e9a3e09
Kirill Karpov <85180621+karpov-kir@users.noreply.github.com>
2024-04-16 10:19:09 +0400

    ci: main workflow (#2)

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,5 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  verbose: true,
   preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'jsdom',
   testMatch: ['<rootDir>/src/**/*.(test).*'],
